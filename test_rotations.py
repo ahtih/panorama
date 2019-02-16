2b621b2efeaa9aa1f4dbb651f54cdad0605d8a3b
Ahti Heinla <ahti@ahti.bluemoon.ee>
2019-02-16 11:46:33 +0200

    Added final test result printout

--- test_rotations.py
+++ test_rotations.py
@@ -3,12 +3,18 @@
 
 import math,panorama,quaternion,image_rotation_optimiser
 
+is_test_ok=True
+
 def test_euler_angles(euler_angles_deg):
+	global is_test_ok
+
 	m=panorama.quaternion_from_kolor_file(*map(math.radians,euler_angles_deg)).to_matrix()
 	output_euler_angles_deg=map(math.degrees,image_rotation_optimiser.matrix_to_kolor_file_angles(m))
 	total_error=sum([abs(a-b) for a,b in zip(euler_angles_deg,output_euler_angles_deg)])
 	print '%6.3f   %3.0f %3.0f %3.0f     %7.2f %7.2f %7.2f' % \
 								((total_error,) + tuple(euler_angles_deg) + tuple(output_euler_angles_deg))
+	if total_error > 0.1:
+		is_test_ok=False
 
 test_euler_angles((0,0,0))
 
@@ -35,3 +41,5 @@ for rot_amount_deg in (10,30,70,-30,-5):
 		test_euler_angles(euler_angles_deg)
 
 	print
+
+print 'TESTS',('OK' if is_test_ok else 'FAILED')
