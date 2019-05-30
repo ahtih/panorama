076be8478bcacc14eb3487747ba2e41d0459659f
Ahti Heinla <ahti@ahti.bluemoon.ee>
2019-05-30 23:37:22 +0300

    Multithreaded S3 upload

--- panorama_aws.py
+++ panorama_aws.py
@@ -114,6 +114,26 @@ def extract_keypoints_and_upload_to_s3(fname,s3_fname,print_verbose=False):
 
 	img.save_to_s3(s3_fname)
 
+def add_images_to_worker_pool(worker_pool,output_fname,image_fnames,print_verbose=False):
+		# Updates processing_batches
+
+	global processing_batches
+
+	processing_batch_key='%016x' % (random.randint(0,2**64-1),)
+	print '%s: Creating processing batch %s with %u images' % (output_fname or '-',processing_batch_key,
+																						len(image_fnames))
+	s3_fnames=[]
+	async_result_objects=[]
+
+	for idx,fname in enumerate(image_fnames):
+		s3_fname=processing_batch_key + '/' + str(idx) + '-' + os.path.basename(fname)
+		s3_fnames.append(s3_fname)
+		async_result_objects.append(worker_pool.apply_async(
+										extract_keypoints_and_upload_to_s3,(fname,s3_fname,print_verbose)))
+
+	processing_batches[processing_batch_key]= \
+							(output_fname,tuple(image_fnames),tuple(s3_fnames),tuple(async_result_objects))
+
 keyword_args={}
 positional_args=[]
 
@@ -139,76 +159,127 @@ if '--output-batch' in keyword_args:
 
 	write_output_file(match_results,keyword_args.get('--output-fname'),True)
 else:
-	image_fnames=positional_args[1:]
 	print_verbose=('--verbose' in keyword_args)
 
-	processing_batch_key='%016x' % (random.randint(0,2**64-1),)
-	print 'Creating processing batch %s with %u images' % (processing_batch_key,len(image_fnames))
-
-	s3_fnames=[]
-
+	lambda_client=panorama.aws_session.client('lambda')
 	worker_pool=multiprocessing.Pool(8*2)
 
-	for idx,fname in enumerate(image_fnames):
-		s3_fname=processing_batch_key + '/' + str(idx) + '-' + os.path.basename(fname)
-		s3_fnames.append(s3_fname)
-		worker_pool.apply_async(extract_keypoints_and_upload_to_s3,(fname,s3_fname,print_verbose))
+	processing_batches=dict()
+
+	panorama_fnames=keyword_args.get('--panoramas-file')
+	if panorama_fnames:
+		if not isinstance(panorama_fnames,list):
+			panorama_fnames=(panorama_fnames,)
+
+		for fname in panorama_fnames:
+			for line in open(fname,'r'):
+				line=line.strip()
+				if not line:
+					continue
+				fields=line.split()
+				if len(fields) < 2:
+					continue
+				add_images_to_worker_pool(worker_pool,fields[0],fields[1:],print_verbose=print_verbose)
+	else:
+		add_images_to_worker_pool(worker_pool,keyword_args.get('--output-fname'),positional_args[1:],
+																			print_verbose=print_verbose)
 
 	if print_verbose:
 		print 'Worker tasks submitted'
 
-	worker_pool.close()
-	worker_pool.join()
+	match_images_started=set()
+	match_images_failed=set()
+	batches_completed=set()
 
-	if print_verbose:
-		print 'Spawning match_images Lambda tasks'
+	while True:
+		something_was_done=False
 
-	lambda_client=panorama.aws_session.client('lambda')
+		if print_verbose:
+			print len(processing_batches),len(match_images_started),len(match_images_failed), \
+																					len(batches_completed)
 
-	lambda_parameters={'function': 'spawn_match_images_tasks',
+		for processing_batch_key in set(processing_batches.keys()) - \
+																match_images_started - match_images_failed:
+			output_fname,image_fnames,s3_fnames,async_result_objects=processing_batches[processing_batch_key]
+			if not min([a.ready() for a in async_result_objects]):
+				continue
+
+			if not min([a.successful() for a in async_result_objects]):
+				print '%s: Errors occurred while processing images' % (output_fname or '-',)
+				match_images_failed.add(processing_batch_key)
+				continue
+
+			if print_verbose:
+				print '%s: Spawning match_images Lambda tasks' % (output_fname or '-',)
+
+			something_was_done=True
+			spawn_start_time=time.time()
+			lambda_parameters={'function': 'spawn_match_images_tasks',
 								'processing_batch_key': processing_batch_key,
 								's3_fnames': s3_fnames,
 								'orig_fnames': image_fnames}
-	invoke_result=lambda_client.invoke(FunctionName='panorama',InvocationType='RequestResponse',
+			try:
+				invoke_result=lambda_client.invoke(FunctionName='panorama',InvocationType='RequestResponse',
 										Payload=json.dumps(lambda_parameters))
-	status_code=invoke_result.get('StatusCode')
+			except Exception as e:
+				print '%s: Lambda invoke failed with: %s' % (output_fname or '-',e)
+				match_images_failed.add(processing_batch_key)
+				continue
 
-	if status_code < 200 or status_code > 299:
-		print 'Lambda invoke failed with:',invoke_result
-		exit(1)
+			status_code=invoke_result.get('StatusCode')
 
-	expected_nr_of_match_results=len(s3_fnames) * (len(s3_fnames)-1) / 2
-	prev_printed_status=0
-
-	prev_update_time=time.time()
-	while True:
-		match_results=get_match_results(processing_batch_key)
-		if len(match_results) != prev_printed_status:
 			if print_verbose:
-				print 'Completed %d of %d pairwise matches' % (len(match_results),expected_nr_of_match_results)
-			prev_printed_status=len(match_results)
-			prev_update_time=time.time()
-		if len(match_results) >= expected_nr_of_match_results:
+				print '%s: Lambda task spawning took %dsec, status %d' % (output_fname or '-',
+															time.time() - spawn_start_time,status_code)
+
+			if status_code is not None and status_code >= 200 and status_code <= 299:
+				match_images_started.add(processing_batch_key)
+			else:
+				match_images_failed.add(processing_batch_key)
+				print '%s: Lambda invoke failed with: %s' % (output_fname or '-',invoke_result)
+
+		for processing_batch_key in match_images_started - batches_completed:
+			s3_fnames=processing_batches[processing_batch_key][2]
+			expected_nr_of_match_results=len(s3_fnames) * (len(s3_fnames)-1) / 2
+
+			match_results=get_match_results(processing_batch_key)
+
+			if len(match_results) < expected_nr_of_match_results:
+				continue
+
+			something_was_done=True
+
+			first_error_text=None
+			error_count=0
+			non_error_results=[]
+
+			for match_result in match_results:
+				error_text=match_result.get('error')
+				if error_text is None:
+					non_error_results.append(match_result)
+				else:
+					error_count+=1
+					if first_error_text is None:
+						first_error_text=error_text
+
+			output_fname=processing_batches[processing_batch_key][0]
+
+			if error_count:
+				print '%s: %d pairwise matches produced errors, first error is here:' % \
+																	(output_fname or '-',error_count)
+				print first_error_text
+
+			if non_error_results:
+				write_output_file(match_results,output_fname,print_verbose)
+				print '%s: Completed' % (output_fname or '-',)
+
+			batches_completed.add(processing_batch_key)
+
+		if len(batches_completed) + len(match_images_failed) >= len(processing_batches):
 			break
-		if time.time() > prev_update_time + 60:
-			print 'Timeout - Lambda functions not progressing'
-			exit(1)
 
-	first_error_text=None
-	error_count=0
-	non_error_results=[]
+		if not something_was_done:
+			time.sleep(3)
 
-	for match_result in match_results:
-		error_text=match_result.get('error')
-		if error_text is None:
-			non_error_results.append(match_result)
-		else:
-			error_count+=1
-			if first_error_text is None:
-				first_error_text=error_text
-
-	if error_count:
-		print '%d pairwise matches produced errors, first error is here:' % (error_count,)
-		print first_error_text
-
-	write_output_file(match_results,keyword_args.get('--output-fname'),print_verbose)
+	worker_pool.close()
+	worker_pool.join()
