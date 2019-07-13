d4ff2a374265fe85e7bb9a9475cd644f99738636
Ahti Heinla <ahti@ahti.bluemoon.ee>
2019-07-13 20:51:56 +0300

    viewer-aframe: Added generate-webapp-buckets-config.js

---
+++ generate-webapp-buckets-config.js
@@ -0,0 +1,72 @@
+module.exports.resources=(serverless) => {
+	var resources={};
+
+	var domains=[	'www.vr-eesti.ee',
+						'vr-eesti.ee',
+					'www.vr-loodus.ee',
+						'vr-loodus.ee',
+					];
+
+	for (var i=0; i < domains.length;i++) {
+		var domain=domains[i];
+
+		resources['WebAppS3Bucket' + domain.replace(/[-.]/g,'')]=
+					{'Type': 'AWS::S3::Bucket',
+					'Properties': {
+					        'BucketName': domain,
+							'AccessControl': 'PublicRead',
+							'WebsiteConfiguration': {
+				        		'IndexDocument': 'index.html',
+				        		'ErrorDocument': 'index.html',
+					        	'RoutingRules': [
+									{ 'RedirectRule': {
+											'HostName': {
+												'Fn::Join': [
+													'',
+													[	{'Ref': 'ApiGatewayRestApi'},
+														'.execute-api.',
+														{'Ref': 'AWS::Region'},
+														'.amazonaws.com'
+														]
+													]
+												},
+											'ReplaceKeyPrefixWith': 'dev/next_image_links/'
+											},
+										'RoutingRuleCondition': {
+											'KeyPrefixEquals': 'next_image_links/'
+											}
+										},
+									{ 'RedirectRule': {
+											'HostName': 'panorama-final-images.s3-website-eu-west-1.amazonaws.com'
+											},
+										'RoutingRuleCondition': {
+											'HttpErrorCodeReturnedEquals': '404'
+											}
+										},
+									]
+								}
+							}
+						};
+		}
+
+/* !!! We'd like to have that block, but with it we get
+			"WebAppS3BucketPolicy - API: s3:PutBucketPolicy Access Denied" during "sls deploy".
+	See https://stackoverflow.com/questions/47931342/how-to-change-s3-bucket-policies-with-cloudformation
+
+    WebAppS3BucketPolicy:
+      Type: AWS::S3::BucketPolicy
+      Properties:
+        Bucket:
+          Ref: WebAppS3Bucket
+        PolicyDocument:
+          Statement:
+            - Sid: AllowPublicRead
+              Effect: Allow
+              Principal: "*"
+              Action:
+              - s3:GetObject
+              Resource: arn:aws:s3:::www.vr-eesti.ee/*
+	*/
+
+	return resources;
+	}
